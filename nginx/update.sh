db8586c9c12dfb9cb4d70b3d40a67925593f10f1
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-01-31 19:41:49 -0600

    Fixed bug where deleting old mounted dir broke file mount

--- nginx/update.sh
+++ nginx/update.sh
@@ -12,15 +12,15 @@ print_header "UPDATING NEXUS REVERSE PROXY CONFIGURATION"
 
 # Remove old configuration files
 print_step "Removing old configuration files"
-rm -rf "${NEXUS_NGINX_ETC_DIR}/conf"
-rm -rf "${NEXUS_NGINX_ETC_DIR}/conf.d"
-rm -rf "${NEXUS_NGINX_ETC_DIR}/snippets"
+rm -rf "${NEXUS_NGINX_ETC_DIR}/conf"/*
+rm -rf "${NEXUS_NGINX_ETC_DIR}/conf.d"/*
+rm -rf "${NEXUS_NGINX_ETC_DIR}/snippets"/*
 
 # Copy fresh configuration from opt
 print_step "Copying updated nginx configuration to /etc/nexus/nginx"
-cp -r "${NEXUS_NGINX_OPT_DIR}/conf" "${NEXUS_NGINX_ETC_DIR}/"
-cp -r "${NEXUS_NGINX_OPT_DIR}/conf.d" "${NEXUS_NGINX_ETC_DIR}/"
-cp -r "${NEXUS_NGINX_OPT_DIR}/snippets" "${NEXUS_NGINX_ETC_DIR}/"
+cp -r "${NEXUS_NGINX_OPT_DIR}/conf"/* "${NEXUS_NGINX_ETC_DIR}/conf/"
+cp -r "${NEXUS_NGINX_OPT_DIR}/conf.d"/* "${NEXUS_NGINX_ETC_DIR}/conf.d/"
+cp -r "${NEXUS_NGINX_OPT_DIR}/snippets"/* "${NEXUS_NGINX_ETC_DIR}/snippets/"
 
 print_success "Configuration files updated successfully"
 print_info "Run ${NEXUS_NGINX_OPT_DIR}/reload.sh to apply changes to the running container"
