836362f5d474f47f0c2f1ed04997b88da77bd091
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-01-31 20:27:30 -0600

    Added status file

---
+++ f2b/status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source "/etc/nexus/conf/conf.sh"
+source "${NEXUS_OPT_DIR}/lib/print.sh"
+
+print_header "FAIL2BAN NGINX JAIL STATUS"
+
+JAILS=("nginx-http-auth" "nginx-bad-request" "nginx-botsearch" "nginx-limit-req")
+
+for jail in "${JAILS[@]}"; do
+    print_step "${jail}"
+    sudo fail2ban-client status "${jail}"
+    print_info ""
+done
