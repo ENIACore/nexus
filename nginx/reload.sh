018e105cbfb6e1175fe7fce40332bc49627ccd61
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-01-31 19:36:24 -0600

    Fixed minor bug in reload

--- nginx/reload.sh
+++ nginx/reload.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+source "/etc/nexus/conf/conf.sh"
+source "${NEXUS_OPT_DIR}/lib/checks.sh"
+source "${NEXUS_OPT_DIR}/lib/print.sh"
+source "${NEXUS_OPT_DIR}/lib/log.sh"
+
 if docker exec nexus-proxy nginx -s reload; then
     print_info "Nginx reloaded successfully"
 else
