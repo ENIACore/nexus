54eff483e071ea256f25c3bf8883fcc410899c8d
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-01-31 20:33:40 -0600

    Temporary print statement

--- install.py
+++ install.py
@@ -119,6 +119,7 @@ def copy_repo_path(repo_path):
     repo_root = Path(repo_path)
 
     # Define paths to copy: (source_relative_path, destination_relative_path)
+    print_step("fuck you")
     paths_to_copy = [
         # Cloudflare files
         ("cloudflare/setup.sh", "cloudflare/setup.sh"),
