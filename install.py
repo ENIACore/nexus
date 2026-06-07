29c25639382820e165d5ce9d973da96ccb1fbc71
Chase Lamkin <CLamkin@wsgc.com>
2026-06-07 11:39:52 -0700

    Updated install script to cleanup existing paths

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import shutil
 import subprocess
 import sys
 from pathlib import Path
@@ -13,7 +13,10 @@ subprocess.run(
     ["curl", "-fsSL", RAW_URL, "-o", str(BOOTSTRAP_SCRIPT)], check=True
 )
 subprocess.run([sys.executable, str(BOOTSTRAP_SCRIPT)], check=True)
-BOOTSTRAP_SCRIPT.unlink(missing_ok=True)
+BOOTSTRAP_SCRIPT.unlink()
+
+if CLONE_PATH.exists():
+    shutil.rmtree(CLONE_PATH)
 
 subprocess.run(["git", "clone", REPO_URL, str(CLONE_PATH)], check=True)
 subprocess.run(
